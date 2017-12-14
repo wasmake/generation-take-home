daeba2d881751eb5c97562af34fd2ef8eabcf21f
Andrew R. l wasmake <andrew.r@akardoo.com>
2017-12-13 21:30:56 -0600

    Update:
    - Fix> Cookies now work with favorites and store some locations to improve the loading of coords.
    
    - Bug> Some cookies wont load in a reload.
    - Bug> Favorites are not stared when reloading.

--- src/YourComponent.js
+++ src/YourComponent.js
@@ -2,22 +2,29 @@ import React, { Component } from 'react';
 
 /*
 * Use this component as a launching-pad to build your functionality.
+* Api key: AIzaSyCVH8e45o3d-5qmykzdhGKd1-3xYua5D2A
 *
 */
+
 export default class YourComponent extends Component {
 
   render() {
     return (
+      <div class="container">
       <div style={divStyle}>
-		  <h1> Put your solution here!</h1>
+      <div id="map" style={divStyle}></div>
+      <div id="over_map" style={overMap}>asdasd</div>
       </div>
+</div>
     );
   }
+
 }
 
 var divStyle = {
-  border: 'red',
-  borderWidth: 2,
-  borderStyle: 'solid',
-  padding: 20
+  height : '100%', width : '100%', top : '0', left : '0', position : 'absolute', zindex : '200',
 };
+
+var overMap = {
+  background: '#fff', height : '10%', width : '30%', top : '0', left : '0', position : 'absolute', zindex : '201',
+}
